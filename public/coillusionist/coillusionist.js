0fd60f0caf4e726b8e3cdf79d010b6481eac2086
Antti Leppä <antti.leppa@foyt.fi>
2014-03-07 12:20:02 +0200

    ** Added support for changing paint

--- public/coillusionist/coillusionist.js
+++ public/coillusionist/coillusionist.js
@@ -80,6 +80,8 @@
         ctx.save();
         try {
           this._getCoIllusionist().CoIllusionist("clipSelection", ctx);
+          this._getCoIllusionist().CoIllusionist("applyPaint", ctx);
+          
           ctx.beginPath();
           ctx.arc(data.screenX, data.screenY, this.size(), 0, 2 * Math.PI, false);
           ctx.fill();
@@ -137,6 +139,219 @@
     }
   });
   
+
+  $.widget("custom.CoIllusionistPaintButton", {
+    options : {},
+    _create : function() {
+      this._paintType = this._pendingType = this.options.type;
+      this._paintValue = this._pendingValue = this.options.value;
+      this._paintPattern = this._pendingPattern = null;
+      this.element.addClass('co-illusionist-paintbar-button')
+        .click($.proxy(this._onClick, this));
+    },
+    
+    paint: function () {
+      switch (this._paintType) {
+        case 'color':
+          return this._paintValue;
+        case 'pattern':
+          return this._paintPattern;
+      }
+    },
+    
+    _onClick: function (event) {
+      this._dialog = $('<div>')
+        .addClass('co-illusionist-paintdialog')
+        .attr('title', 'Paint');
+      var tabsElement = $('<div>');
+
+      var prefix = new Date().getTime() + '-';
+      var colorTabId = prefix + 'color';
+      var patternTabId = prefix + 'pattern';
+
+      this._tabLabels = $('<ul>');
+      this._tabs = $('<div>');
+
+      tabsElement.append(this._tabLabels);
+      tabsElement.append(this._tabs);
+      this._dialog.append(tabsElement);
+      
+      $('<li>').append($('<a>')
+        .text('Color')
+        .attr('href', '#' + colorTabId))
+        .appendTo(this._tabLabels);
+      
+      $('<li>').append(
+        $('<a>')
+          .text('Pattern')
+          .attr('href', '#' + patternTabId))
+          .appendTo(this._tabLabels);
+
+      var colorTabContent = $('<div>')
+        .attr('id', colorTabId)
+        .appendTo(tabsElement);
+      
+      var patternTabContent = $('<div>')
+        .attr('id', patternTabId)
+        .appendTo(tabsElement);
+      
+      patternTabContent.append($('<div>').addClass('co-illusionist-paintdialog-patterns'));
+      
+      tabsElement.tabs();
+      this._dialog.dialog({
+        resizable: false,
+        modal: true,
+        buttons: {
+          "Ok": $.proxy(function () {
+            this._applyPaint();
+            $(this._dialog).dialog("close");
+          }, this),
+          "Cancel": $.proxy(function () {
+            this._cancelPaint();
+            $(this._dialog).dialog("close");
+          }, this)
+        }
+      });
+      
+      var color = this._paintType === 'color' ? this._paintValue : '#000';
+      var colorInput = $('<input>').appendTo(colorTabContent).val(color);
+      $('<div>')
+        .addClass('co-illusionist-paintdialog-preview')
+        .append($('<span>')
+          .css({
+            'backgroundColor': color,
+            'backgroundImage': 'none'
+          })
+        )
+        .appendTo(colorTabContent);
+      
+      colorInput.spectrum({
+        flat: true,
+        showInput: false,
+        showButtons: false,
+        showAlpha: true,
+        clickoutFiresChange: true,
+        move: $.proxy(function(color) {
+          this._setPaint(true, 'color', color.toRgbString());
+        }, this)
+      });
+      
+      $.ajax('/patterns/', {
+        success: $.proxy(function (data, textStatus, jqXHR) {
+          for (var i = 0, l = data.length; i < l; i++) {
+            var path = data[i].path;
+            var name = data[i].name;
+            
+            $('<div>')
+              .addClass('co-illusionist-paintdialog-pattern')
+              .data('path', path)
+              .append($('<img>')
+                .attr('src', path)
+                .attr('title', name)
+                .click($.proxy(this._onPatternClick, this))
+              )
+              .appendTo(patternTabContent.find('.co-illusionist-paintdialog-patterns'));
+          }
+        }, this)
+      });
+    },
+    
+    _getCoIllusionist: function () {
+       return this.element.closest('.co-illusionist');
+    },
+    
+    _cancelPaint: function () {
+      this._setPaint(false, this._paintType, this._paintValue);
+    },
+
+    _applyPaint: function () {
+      this._setPaint(false, this._pendingType, this._pendingValue);
+    },
+    
+    _setPaint: function (pending, type, value) {
+      switch (type) {
+        case 'color':
+          this.element.css({
+            'backgroundColor': value,
+            'backgroundImage': 'none'
+          });
+
+          if (pending) {
+            this._pendingType = type;
+            this._pendingValue = value;
+            this._pendingPattern = null;
+          } else {
+            this._paintType = type;
+            this._paintValue = value;
+            this._paintPattern = null;
+          }
+          
+          this._dialog.find('span').css("backgroundColor", value);
+        break;
+        case 'pattern':
+          var image = new Image();
+          image.onload = $.proxy(function () {
+            this.element.css({
+              'backgroundColor': null,
+              'backgroundImage': 'url(' + value + ')'
+            });
+            
+            if (pending) {
+              this._pendingType = type;
+              this._pendingValue = value;
+              this._pendingPattern = this._getCoIllusionist().CoIllusionist("createPattern", image);
+            } else {
+              this._paintType = type;
+              this._paintValue = value;
+              this._paintPattern = this._getCoIllusionist().CoIllusionist("createPattern", image);
+            }
+            
+            this._dialog.find('span').css("backgroundColor", '#000');
+          }, this);
+          
+          image.src = value;
+        break;
+      }
+    },
+    
+    _onPatternClick: function (event) {
+      this._setPaint(true, 'pattern', $(event.target).attr('src'));
+    },
+
+    _destroy : function() {
+    }
+  });
+  
+  $.widget("custom.CoIllusionistPaintBar", {
+    _create : function() {
+      this._fill = $('<div>').CoIllusionistPaintButton({
+        'type': 'color',
+        'value': 'rgba(0, 0, 0, 1)'
+      });
+      
+      this._stroke = $('<div>').CoIllusionistPaintButton({
+        'type': 'color',
+        'value': 'rgba(255, 255, 255, 0)'
+      });
+      
+      this.element.addClass('co-illusionist-paintbar')
+        .append($('<div>').addClass('co-illusionist-paintbar-button-container').append(this._fill))
+        .append($('<div>').addClass('co-illusionist-paintbar-button-container').append(this._stroke));
+    },
+    
+    apply: function (context) {
+      context.strokeStyle = this._stroke.CoIllusionistPaintButton('paint');
+      context.fillStyle = this._fill.CoIllusionistPaintButton('paint');
+    },
+    
+    _getCoIllusionist: function () {
+       return this.element.closest('.co-illusionist');
+    },
+    
+    _destroy : function() {
+    }
+  });
+  
   $.widget("custom.CoIllusionistToolbar", {
     _create : function() {
       this.element.addClass('co-illusionist-toolbar');
@@ -183,6 +398,8 @@
         .append($('<div>').CoIllusionistToolSelectRect())
         .appendTo(this.element);
       
+      this.element.append($('<div>').CoIllusionistPaintBar());
+      
       this.drawOffscreen($.proxy(function (ctx, done) {
         ctx.drawImage(this.options.image.get(0), 0, 0);
         done();
@@ -220,6 +437,10 @@
       }
     },
     
+    applyPaint: function (ctx) {
+      $(this.element).find('.co-illusionist-paintbar').CoIllusionistPaintBar('apply', ctx);
+    },
+    
     requestFlip: function () {
       this._flipToScreen();
     },
@@ -241,6 +462,11 @@
       }
 
       return this._selection;
+    },
+    
+    createPattern: function (image) {
+      var ctx = this._offscreen.get(0).getContext("2d");
+      return ctx.createPattern(image, "repeat");
     },
 
     _onOffscreenBeforeDraw: function () {
