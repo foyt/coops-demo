ae061f03d07cba657c18e100a5061e4d1cd189eb
Antti Leppä <antti.leppa@foyt.fi>
2014-03-08 20:54:56 +0200

    ** Removed jquery support from pixastic.js

--- public/pixastic/pixastic.js
+++ public/pixastic/pixastic.js
@@ -434,33 +434,6 @@ var Pixastic = (function() {
 
 })();
 /*
- * Pixastic Lib - jQuery plugin
- * Copyright (c) 2008 Jacob Seidelin, jseidelin@nihilogic.dk, http://blog.nihilogic.dk/
- * License: [http://www.pixastic.com/lib/license.txt]
- */
-
-if (typeof jQuery != "undefined" && jQuery && jQuery.fn) {
-	jQuery.fn.pixastic = function(action, options) {
-		var newElements = [];
-		this.each(
-			function () {
-				if (this.tagName.toLowerCase() == "img" && !this.complete) {
-					return;
-				}
-				var res = Pixastic.process(this, action, options);
-				if (res) {
-					newElements.push(res);
-				}
-			}
-		);
-		if (newElements.length > 0)
-			return jQuery(newElements);
-		else
-			return this;
-	};
-
-};
-/*
  * Pixastic Lib - Blend - v0.1.1
  * Copyright (c) 2008 Jacob Seidelin, jseidelin@nihilogic.dk, http://blog.nihilogic.dk/
  * License: [http://www.pixastic.com/lib/license.txt]
