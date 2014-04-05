f5fd177c2d42ea3f21d1e134de3ff46cf148473d
Antti Leppä <antti.leppa@foyt.fi>
2014-04-05 13:10:42 +0300

    ** Added removeCollaborator mehtod into collaborators js widget

--- public/scripts/collaborators.js
+++ public/scripts/collaborators.js
@@ -6,13 +6,20 @@
     _create: function () {
     },
     
-    addCollaborator: function (name, email) {
+    addCollaborator: function (sessionId, name, email) {
       $(this.element).append(
         $('<div>').collaborator({
+          sessionId: sessionId,
           name: name,
           email: email
         })
       );
+    },
+    
+    removeCollaborator: function (sessionId) {
+      $(this.element).find('#collaborator-' + sessionId).hide("blind", function () {
+        $(this).remove();
+      });
     },
     
     _destroy : function() {
@@ -29,14 +36,18 @@
     _create: function () {
       $(this.element)
         .addClass('collaborator')
+        .attr({
+          id: 'collaborator-' + this.options.sessionId,
+        })
         .append($('<img>')
-          .attr("title", this.options.name)
-          .attr('src', '//www.gravatar.com/avatar/' +
-              hex_md5(this.options.email) +
+          .attr({
+            title: this.options.name,
+            src: '//www.gravatar.com/avatar/' + hex_md5(this.options.email) +
               '?d=' + this.options.gravatarDefault +
               '&r=' + this.options.gravatarRating +
-              "&s=" + this.options.gravatarSize)
-          );
+              "&s=" + this.options.gravatarSize
+          })
+        );
     },
     
     _destroy : function() {
