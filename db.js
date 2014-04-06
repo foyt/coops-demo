930be5c7bfd277863f2b1c243182238cf28e452b
Antti Leppä <antti.leppa@foyt.fi>
2014-04-06 09:23:38 +0300

    ** Added new collection 'filesessions' into database

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 (function() {
   var settings = require('./settings.json');
-  var collections = ["users", "useridentities", "useremails", "sessions", "files", "filerevisions", "fileusers"];
+  var collections = ["users", "useridentities", "useremails", "sessions", "files", "filerevisions", "fileusers", "filesessions"];
   var db = require("mongojs").connect(settings.mongo.url, collections);
   module.exports = db;
 }).call(this);
