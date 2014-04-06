a7bc174403f269b4cc4e159a0dd009267a5d5cde
Antti Leppä <antti.leppa@foyt.fi>
2014-04-06 09:26:32 +0300

    ** Changed sessionsevents CKEditor plugin to fire initial session event data on session start

--- public/ckplugins/coops-sessionevents/plugin.js
+++ public/ckplugins/coops-sessionevents/plugin.js
@@ -5,10 +5,21 @@
     $: function(editor) {
       this._editor = editor;
       this._editor.on("CoOPS:SessionStart", this._onSessionStart, this);
+      this._editor.on("CoOPS:BeforeSessionStart", this._onBeforeSessionStart, this);
+      this._initialSessionEvents = null;
     },
     proto : {
       
-      _onSessionStart: function () {
+      _onBeforeSessionStart: function (event) {
+        this._initialSessionEvents = event.data.joinData.extensions.sessionEvents;
+      },
+      
+      _onSessionStart: function (event) {
+        if (this._initialSessionEvents) {
+          this._handleSessionEvents(this._initialSessionEvents);
+          this._initialSessionEvents = null;
+        }
+        
         this._editor.on("CoOPS:PatchReceived", this._onPatchReceived, this, null, 9999);
       },
 
@@ -17,24 +28,28 @@
         var sessionId = event.data.sessionId;
         
         if (data.extensions && data.extensions.sessionEvents) {
-          for (var i = 0, l = data.extensions.sessionEvents.length; i < l; i++) {
-            var sessionEvent = data.extensions.sessionEvents[i];
-            switch (sessionEvent.status) {
-              case 'OPEN':
-                this._editor.fire("CoOPS:CollaboratorJoined", {
-                  displayName: sessionEvent.displayName,
-                  email: sessionEvent.email,
-                  sessionId: sessionId
-                });
-              break;
-              case 'CLOSE':
-                this._editor.fire("CoOPS:CollaboratorLeft", {
-                  displayName: sessionEvent.displayName,
-                  email: sessionEvent.email,
-                  sessionId: sessionId
-                });
-              break;
-            }
+          this._handleSessionEvents(data.extensions.sessionEvents);
+        }
+      },
+      
+      _handleSessionEvents: function (sessionEvents) {
+        for (var i = 0, l = sessionEvents.length; i < l; i++) {
+          var sessionEvent = sessionEvents[i];
+          switch (sessionEvent.status) {
+            case 'OPEN':
+              this._editor.fire("CoOPS:CollaboratorJoined", {
+                displayName: sessionEvent.displayName,
+                email: sessionEvent.email,
+                sessionId: sessionEvent.sessionId
+              });
+            break;
+            case 'CLOSE':
+              this._editor.fire("CoOPS:CollaboratorLeft", {
+                displayName: sessionEvent.displayName,
+                email: sessionEvent.email,
+                sessionId: sessionEvent.sessionId
+              });
+            break;
           }
         }
       }
